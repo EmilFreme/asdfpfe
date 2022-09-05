f64053f25a9e07071cedb36d83e50756a557eec3
Ana Clara <anaclara.2001@hotmail.com>
2022-09-05 18:26:38 -0300

    remove the polygon mesh

---
+++ Assets/Scripts/meshes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+
+public class meshes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        // CoreServices.SpatialAwarenessSystem.Enable();
+        // SpatialMappingManager.Instance.DrawVisualMeshes = false;
+        
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
