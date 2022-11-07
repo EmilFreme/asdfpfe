c125d6a8f05975e8d817f6824a321ec66c7698bb
anaclaracf <anaclara.2001@hotmail.com>
2022-11-07 18:05:29 -0300

    scripts

--- Assets/socket_client.cs
+++ Assets/socket_client.cs
@@ -8,20 +8,29 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using System.Globalization;
+using System.Threading.Tasks;
 
 public class socket_client : MonoBehaviour {  	
 	#region private members 	
 	private TcpClient socketConnection; 	
 	private Thread clientReceiveThread; 	
+	Task m_Task;
     private int data_type;
+	public string verText;
+	public TextMesh verificationText;
+	Transform Root;
 	#endregion  	
 	// Use this for initialization 	
 	void Start () {
+		verText = "not yet";
 		ConnectToTcpServer();    
-        data_type=0; 
+        data_type=1; 
 	}  	
 	// Update is called once per frame
 	void Update () {         
+		Root = transform;
+		verificationText.text = verText;
 		if (Input.GetKeyDown(KeyCode.Space)) {             
 			SendMessage();         
 		}     
@@ -30,7 +39,12 @@ public class socket_client : MonoBehaviour {
 
 	private void ConnectToTcpServer () { 		
 		try {  			
-			clientReceiveThread = new Thread (new ThreadStart(ListenForData)); 			
+			// ListenForData();
+			// m_Task = Task.Run((Action) ListenForData);
+			// m_Task = new Task(() => ListenForData());
+			// m_Task.Start();
+
+			clientReceiveThread = new Thread (ListenForData); 			
 			clientReceiveThread.IsBackground = true; 			
 			clientReceiveThread.Start();  		
 		} 		
@@ -41,9 +55,10 @@ public class socket_client : MonoBehaviour {
   
 	private void ListenForData() { 		
 		try { 			
-			socketConnection = new TcpClient("192.168.0.42", 9051);  			
-			Byte[] bytes = new Byte[1024];             
-			while (true) { 				
+			 			
+			socketConnection = new TcpClient("192.168.0.42", 9051); 
+			Byte[] bytes = new Byte[1024];           
+			while (true && socketConnection.Connected) { 				
 				// Get a stream object for reading 				
 				using (NetworkStream stream = socketConnection.GetStream()) { 					
 					int length; 					
@@ -54,19 +69,44 @@ public class socket_client : MonoBehaviour {
 						var incommingData = new byte[lengthData]; 						
 						Array.Copy(bytes, 0, incommingData, 0, lengthData); 						
 						// Convert byte array to string message. 						
-
+						verText="foiii";
 						// Debug.Log("server message received as: " + serverMessage); 	
-                        if(data_type==0){
+                        if(data_type==1){
 
                             string translation = Encoding.ASCII.GetString(incommingData); 
-                            Debug.Log("Translation: " + translation); 	
-                            data_type=1;
+							translation=translation.Remove(0, 1);
+							translation=translation.Remove((translation.Length-1), 1);
+							string[] pos_trans;
+							pos_trans=translation.Split(',');
+							float x_trans = float.Parse(pos_trans[0], CultureInfo.InvariantCulture.NumberFormat);
+							float y_trans= float.Parse(pos_trans[1], CultureInfo.InvariantCulture.NumberFormat);
+							float z_trans = float.Parse(pos_trans[2], CultureInfo.InvariantCulture.NumberFormat);
+							// Root.localPosition = new Vector3(-(float)y_trans * 0.001f, (float)z_trans * 0.001f, (float)x_trans * 0.001f);
+                            Debug.Log("Translation x : " + x_trans);
+                            Debug.Log("Translation y : " + y_trans); 	
+                            Debug.Log("Translation z : " + z_trans); 	
+
+                            data_type=0;
 							Array.Clear(bytes, 0, bytes.Length);
 
                         }else{
                             string rotation = Encoding.ASCII.GetString(incommingData); 
-                            Debug.Log("Rotation: " + rotation); 	
-                            data_type=0;
+							rotation=rotation.Remove(0, 1);
+							rotation=rotation.Remove((rotation.Length-1), 1);
+							//  Debug.Log("Rotation x : " + rotation);
+							string[] pos_rot;
+							pos_rot=rotation.Split(','); 
+							// float x_rot = float.Parse(pos_rot[0], CultureInfo.InvariantCulture.NumberFormat);
+							// float y_rot= float.Parse(pos_rot[1], CultureInfo.InvariantCulture.NumberFormat);
+							// float z_rot = float.Parse(pos_rot[2], CultureInfo.InvariantCulture.NumberFormat);
+							// float a_rot = float.Parse(pos_rot[3], CultureInfo.InvariantCulture.NumberFormat);
+							// // Root.localRotation = new Quaternion((float)y_rot, -(float)z_rot, -(float)x_rot, (float)a_rot);
+                            // Debug.Log("Rotation x : " + x_rot);
+                            // Debug.Log("Rotation y : " + y_rot); 	
+                            // Debug.Log("Rotation z : " + z_rot); 	
+ 							// Debug.Log("Rotation a : " + a_rot); 	
+                          	
+                            data_type=1;
 							Array.Clear(bytes, 0, bytes.Length);
                         }			
 					} 				
